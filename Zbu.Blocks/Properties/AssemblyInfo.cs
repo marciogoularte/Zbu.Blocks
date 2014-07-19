3718b139d46f443a28481dd36c540ce724e3c54c
Stephan <stephane@umbraco.dk>
2014-07-19 14:52:13 +0200

    Build NuGet package

--- Zbu.Blocks/Properties/AssemblyInfo.cs
+++ Zbu.Blocks/Properties/AssemblyInfo.cs
@@ -5,34 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Zbu.Blocks")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Zbu.Blocks")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyTitle("ZpqrtBnk Umbraco Blocks")]
+[assembly: AssemblyDescription("ZpqrtBnk Umbraco Blocks.")]
 [assembly: Guid("36c898f6-9c9a-4e9b-9371-3d74db27a8e2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Zbu.Blocks.Tests")]
---
+++ Zbu.Blocks/Properties/CommonInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ZpqrtBnk Umbraco Blocks")]
+[assembly: AssemblyCompany("Pilotine - ZpqrtBnk")]
+[assembly: AssemblyCopyright("Copyright © Pilotine - ZpqrtBnk 2013-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// versionning
+// nuget/semver: major.minor.patch [-xxx]
+// assembly: major.minor.patch.build
+// nuget sorts the -xxx alphabetically
+
+// nuget            assembly
+// 1.8.0-alpha001   1.8.0.0
+// 1.8.0-alpha002   1.8.0.1
+// 1.8.0-alpha      1.8.0.2
+// 1.8.0-beta001    1.8.0.3
+// 1.8.0-beta002    1.8.0.4
+// 1.8.0-beta       1.8.0.5
+// 1.8.0            1.8.0.6
+
+// Vsix
+// Also need to 
+
+// Assembly
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
+// NuGet Package
+[assembly: AssemblyInformationalVersion("1.0.0-alpha001")]
+// Do not remove this line.
