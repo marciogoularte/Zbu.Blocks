e047d9c02270ff40876ecb0d2340424b895825c5
Stephan <stephane@umbraco.dk>
2014-09-19 11:23:23 +0200

    Version 1.0.0-alpha004

--- Zbu.Blocks/Properties/CommonInfo.cs
+++ Zbu.Blocks/Properties/CommonInfo.cs
@@ -28,9 +28,9 @@ using System.Runtime.InteropServices;
 // Also need to 
 
 // Assembly
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 
 // NuGet Package
-[assembly: AssemblyInformationalVersion("1.0.0-alpha003")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha004")]
 // Do not remove this line.
